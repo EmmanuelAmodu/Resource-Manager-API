cca6d53642e76ee5b9cf83847923deed90d23de8
Amodu Emmanuel <aWX536413@china.huawei.com>
2019-03-20 12:10:56 +0100

    added methods for checking user info and checking if user is logged in

--- src/server/Services/AuthenticationService.ts
+++ src/server/Services/AuthenticationService.ts
@@ -10,20 +10,8 @@ export class AuthenticationService {
         return this.getUserDetails();
     }
 
-    private openDB(table: string, body: any): OpenDBConnector {
-        const openDB: OpenDBConnector = new OpenDBConnector(table, body);
-        return openDB;
-    }
-
-    private getUserDetails(){
-        const that = this;
-        return new Promise<any>(function(resolve, reject) {
-            that.openDB('usertable', that.auth).read().then(res => {
-                resolve(res);
-            }).catch(err => {
-                reject(err)
-            });
-        });
+    public get isloggedIn() {
+        return this.checkIfLoggedIn(this.auth);
     }
 
     public login() {
@@ -40,10 +28,6 @@ export class AuthenticationService {
         })
     }
 
-    private checkIfLoggedIn(usertoken: {username: string, token: string}) {
-
-    }
-
     public logout(usertoken?) {
         const that = this;
         return new Promise<any>(function(resolve, reject) {
@@ -52,6 +36,43 @@ export class AuthenticationService {
             }).catch(err => {
                 reject({"status": "login failed"});
             });
+        });
+    }
+
+    private openDB(table: string, body: any): OpenDBConnector {
+        return new OpenDBConnector(table, body);
+    }
+
+    private createUser() {
+        const that = this;
+        return new Promise<boolean>(function(resolve, reject) {
+            that.openDB('usertable', that.auth).create()
+                .then(res => resolve(true))
+                .catch(res => reject(res))
+        });
+    }
+
+    private getUserDetails(){
+        const that = this;
+        return new Promise<any>(function(resolve, reject) {
+            that.isloggedIn.then(res => {
+                res ? that.openDB('usertable', that.auth).read()
+                .then(res => resolve(res))
+                    .catch(err => reject(err)) :
+                reject({"err": "authentication failed"});
+            })
+
+        });
+    }
+
+    private checkIfLoggedIn(usertoken: {username: string, token: string}) {
+        const that = this;
+        return new Promise<boolean>(function(resolve, reject) {
+            usertoken.token ? 
+            that.validateToken({username: usertoken.username}, [usertoken])
+                .then(res => res.username == usertoken.username ? resolve(true) : resolve(false))
+                    .catch(err =>  reject(err)) :
+            reject({"err": "token not defined"});
         });
     }
 
@@ -70,19 +91,15 @@ export class AuthenticationService {
     private validateToken(update, data) {
         const that = this;
         return new Promise<any>(function(resolve, reject) {
-            console.log(data, data.length);
             if(data.length == 1) {
                 let expiry = parseInt(data[0].expiry);
-                console.log("new Date().getTime() >= expiry", new Date().getTime() >= expiry);
                 if (new Date().getTime() >= expiry) {
-                    that.logout(update).then(res => {
-                        resolve({"err": "token expired"});
-                    });
+                    that.logout(update);
+                    reject({"err": "token expired"});
                 } else resolve(data);
             }
             else if (data.length > 1) {
                 that.deleteToken(update).then(dres => {
-                    console.log("dres", dres);
                     that.setToken(update).then(res => {
                         resolve(res);
                     }).catch(err => reject({"status": "login failed at getToken(), there is more than one token for this user, trying to delete all and set another one"}));
@@ -95,7 +112,6 @@ export class AuthenticationService {
     }
 
     private deleteToken(usertoken: any, callback?) {
-        console.log("delete");
         const that = this;
         return new Promise<UserToken>(function(resolve, reject) {
             that.openDB("usertoken", usertoken).delete().then(mes => {
@@ -110,7 +126,6 @@ export class AuthenticationService {
             crypto.randomBytes(48, function(err, buffer) {
                 usertoken.expiry = new Date().getTime() + (4 * 60 * 60 * 1000)
                 usertoken.token = buffer.toString('hex');
-                console.log(usertoken);
                 that.openDB("usertoken", usertoken).create().then(res => {
                     if (res.ok == 1) resolve(usertoken);
                 }).catch(err => reject({"status": "login failed at setToken(), please try again", err: err}));
