a18267a604cc5137f2770ffb8d41bd5e83da4ff9
Amodu Emmanuel <aWX536413@china.huawei.com>
2019-03-28 19:52:15 +0100

    more services added

---
+++ src/server/Services/FormService.ts
@@ -0,0 +1,8 @@
+import { OpenDBConnector } from '../../OpenDBConnector/OpenDBConnector';
+import { AuthenticationService } from './AuthenticationService';
+
+export class FormService {
+
+    constructor(private params: any){}
+
+}
---
+++ src/server/Services/GridDataService.ts
@@ -0,0 +1,25 @@
+import { OpenDBConnector } from '../../OpenDBConnector/OpenDBConnector';
+import { AuthenticationService } from './AuthenticationService';
+
+export class GridDataService {
+    private queryObject: any;
+
+    constructor(private params: any){}
+
+    private openDB(table: string, body: any): OpenDBConnector {
+        return new OpenDBConnector(table, body);
+    }
+
+    public get data() {
+        return this.getData();
+    }
+
+    private getData() {
+        this.openDB(this.params.entity, this.params.query).read()
+    }
+
+    private sqlInterpreter(){
+
+    }
+
+}
